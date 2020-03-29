a7a98b52c37f7026e8bcb3f1b508a82f22f25493
qiyei2015 <1273482124@qq.com>
2020-03-29 21:04:00 +0800

    首页开发-首页框架01

--- app/src/main/java/com/qiyei/music/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/qiyei/music/ui/activity/HomeActivity.kt
@@ -2,12 +2,18 @@ package com.qiyei.music.ui.activity
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
-import android.view.View
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentStatePagerAdapter
 import com.qiyei.common.ui.activity.BaseActivity
 import com.qiyei.music.R
+import com.qiyei.music.ui.fragment.FindFragment
+import com.qiyei.music.ui.fragment.FriendFragment
+import com.qiyei.music.ui.fragment.MyFragment
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlin.math.log
 
 /**
  * @author Created by qiyei2015 on 2020/3/27.
@@ -17,6 +23,9 @@ import kotlinx.android.synthetic.main.activity_home.*
  */
 class HomeActivity : BaseActivity() {
 
+
+    private val names = listOf<String>("我的","发现","朋友")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -24,8 +33,38 @@ class HomeActivity : BaseActivity() {
         initView()
     }
 
-
     override protected fun initView():Unit{
+
+        slide_toggle_imv.setOnClickListener {
+            Log.i("HomeActivity","start=" + draw_layout.isDrawerOpen(GravityCompat.START))
+            if (!draw_layout.isDrawerOpen(GravityCompat.START)){
+                draw_layout.openDrawer(GravityCompat.START)
+            } else {
+                draw_layout.closeDrawer(GravityCompat.START)
+            }
+        }
+
+        home_view_pager.adapter = object : FragmentStatePagerAdapter(supportFragmentManager,BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+            override fun getItem(position: Int): Fragment {
+                val name = names[position]
+                when (name) {
+                    "我的" -> return MyFragment.newInstance("","");
+                    "发现" -> return FindFragment()
+                    "朋友" -> return FriendFragment()
+                    else -> return FindFragment()
+                }
+            }
+
+            override fun getPageTitle(position: Int): CharSequence? {
+                return names[position]
+            }
+
+            override fun getCount(): Int {
+                return names.size;
+            }
+        }
+        home_tab_layout.setupWithViewPager(home_view_pager)
 
     }
 }
---
+++ app/src/main/java/com/qiyei/music/ui/fragment/FriendFragment.java
@@ -0,0 +1,15 @@
+package com.qiyei.music.ui.fragment;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * @author Created by qiyei2015 on 2020/3/29.
+ * @version: 1.0
+ * @email: 1273482124@qq.com
+ * @description:
+ */
+public class FriendFragment extends Fragment {
+
+
+
+}
