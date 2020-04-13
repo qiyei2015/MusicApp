e74443a22bbc6a1a67066147698a2bb16c874279
qiyei2015 <1273482124@qq.com>
2020-04-14 00:12:28 +0800

    音乐播放器-播放服务开发02

--- lib_audio/src/main/java/com/qiyei/audio/component/activity/MusicPlayerActivity.kt
+++ lib_audio/src/main/java/com/qiyei/audio/component/activity/MusicPlayerActivity.kt
@@ -6,11 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import com.qiyei.audio.R
 import com.qiyei.audio.api.AudioPlayerManager
 import com.qiyei.audio.core.PlayMode
+import com.qiyei.audio.model.AudioBean
 import kotlinx.android.synthetic.main.audio_activity_music_player.*
 
-class MusicPlayerActivity : AppCompatActivity() , View.OnClickListener{
+class MusicPlayerActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var mPlayMode:PlayMode = PlayMode.LOOP
+    private var mPlayMode: PlayMode = PlayMode.LOOP
+    private var isPlay: Boolean = false
+
+    private var mCurrentBean: AudioBean? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,20 +24,31 @@ class MusicPlayerActivity : AppCompatActivity() , View.OnClickListener{
     }
 
     override fun onClick(v: View?) {
-        when (v?.id){
+        when (v?.id) {
+            R.id.favourite_btn_imv -> toggleMusicFavourite()
             R.id.play_mode_btn_imv -> togglePlayMode()
+            R.id.prev_btn_imv -> playPrev()
+            R.id.play_btn_imv -> toggleMusicPlay()
+            R.id.next_btn_play -> playNext()
         }
     }
 
-    private fun initData(){
-
+    private fun initData() {
+        isPlay = false
+        mCurrentBean = AudioBean()
     }
 
-    private fun initView(){
+    private fun initView() {
+        play_btn_imv.setImageDrawable(getDrawable(R.mipmap.icon_audio_music_pause))
+
+        favourite_btn_imv.setOnClickListener(this)
         play_mode_btn_imv.setOnClickListener(this)
+        prev_btn_imv.setOnClickListener(this)
+        play_btn_imv.setOnClickListener(this)
+        next_btn_play.setOnClickListener(this)
     }
 
-    private fun togglePlayMode(){
+    private fun togglePlayMode() {
         when (mPlayMode) {
             PlayMode.REPEAT -> {
                 mPlayMode = PlayMode.LOOP
@@ -51,5 +66,34 @@ class MusicPlayerActivity : AppCompatActivity() , View.OnClickListener{
             }
         }
         AudioPlayerManager?.getInstance().setPlayMode(mPlayMode)
+    }
+
+    private fun toggleMusicFavourite() {
+        favourite_btn_imv.isActivated = !favourite_btn_imv.isActivated
+        if (favourite_btn_imv.isActivated) {
+            AudioPlayerManager.getInstance().addFavourite(mCurrentBean!!)
+        } else {
+            AudioPlayerManager.getInstance().removeFavourite(mCurrentBean!!)
+        }
+    }
+
+    private fun playPrev() {
+        AudioPlayerManager.getInstance().prev()
+    }
+
+    private fun toggleMusicPlay() {
+        if (isPlay) {
+            isPlay = false
+            play_btn_imv.setImageDrawable(getDrawable(R.mipmap.icon_audio_music_pause))
+            AudioPlayerManager.getInstance().pause()
+        } else {
+            isPlay = true
+            play_btn_imv.setImageDrawable(getDrawable(R.mipmap.icon_audio_music_play))
+            AudioPlayerManager.getInstance().resume()
+        }
+    }
+
+    private fun playNext() {
+
     }
 }
